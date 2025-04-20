e107bda0dcd607db3a4e5e97b5b10ac555502fdd
dennisw002 <dennis.wong002@umb.edu>
2025-04-20 01:48:46 -0400

    auto-connect wifi
    Now, when running the python program, it automatically connects to the current wifi network.

--- weather_analysis/src/__main__.py
+++
@@ -1,6 +0,0 @@
-import os
-
-# Method 1: Using os.path to get the root directory
-
-parent_dir = os.path.dirname(os.getcwd()) + "/data/bostonweather.csv"
-print(f"Root directory (using os.path): {parent_dir}")
